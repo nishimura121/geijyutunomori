a3bcce120b55967363a66f0cb99b756e7162932f
nishimura <hyugarikudo11@gmail.com>
2020-05-16 16:55:34 +0900

    edit migration file

--- db/migrate/20200508152138_add_name_to_users.rb
+++ db/migrate/20200508152138_add_name_to_users.rb
@@ -1,6 +1,5 @@
 class AddNameToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :name, :string
     add_column :users, :age, :integer
     add_column :users, :address, :string
     add_column :users, :hp_address, :string
