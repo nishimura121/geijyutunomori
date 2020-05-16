bff6d1959922f8fd6f261873545cac750360d225
nishimura <hyugarikudo11@gmail.com>
2020-05-16 17:06:04 +0900

    reedit migration

--- db/migrate/20200508115047_create_comments.rb
+++ db/migrate/20200508115047_create_comments.rb
@@ -2,8 +2,10 @@ class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
       t.string :content
-      t.references :item, type: :integer, foreign_key: true
-      t.references :user, type: :integer, foreign_key: true
+      t.integer :item_id
+      t.integer :user_id
+      #t.references :item, type: :integer, foreign_key: true
+      #t.references :user, type: :integer, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_14_112109) do
+ActiveRecord::Schema.define(version: 2020_05_13_072540) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -84,9 +84,9 @@ ActiveRecord::Schema.define(version: 2020_05_14_112109) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
     t.integer "age"
     t.string "address"
     t.string "hp_address"
