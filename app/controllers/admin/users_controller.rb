eed62acf19af3abdd95184958759e49094331322
vagrant <vagrant@localhost.localdomain>
2020-05-29 10:19:30 +0000

    [fix]adminview修正

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -11,6 +11,15 @@ class Admin::UsersController < ApplicationController
   	@user = User.find(params[:id])
     @comments = @user.comments.page(params[:page]).per(10)
     @items = @user.items.page(params[:page]).per(10)
+
+    #レベル機能
+    @exp1 = Item.where(user_id: params[:id]).all.count * 10
+    @exp2 = Comment.where(user_id: params[:id]).all.count
+    @exp3 = @user.follower.count
+    @exp4 = @user.followed.count
+    @exp5 = @user.favorites.count
+    @user.exp_sum = @exp1.to_i + @exp2.to_i + @exp3.to_i + @exp4.to_i + @exp5.to_i
+    @user.level = @user.exp_sum.to_i / 10 + 1
   end
 
   def edit
